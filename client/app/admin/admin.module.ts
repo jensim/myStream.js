de08d62f630a23aec05ab851bb102afd87503f8e
Jens Brimberg <jens.brimberg@gmail.com>
2016-05-31 06:22:58 +0200

    Yeoman scaffolded a fullstack angular

---
+++ client/app/admin/admin.module.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myStreamJsApp.admin', [
+  'myStreamJsApp.auth',
+  'ui.router'
+]);
---
+++ client/app/main/main.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('myStreamJsApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
