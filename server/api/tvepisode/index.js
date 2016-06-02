4bd040edb17df1c5e71ff368a0d501e03af21ba2
Jens Brimberg <jens.brimberg@gmail.com>
2016-06-02 18:28:01 +0200

    Revert "Added restrictions to the server apis"
    This reverts commit bc426973257ba98f42b9f26f1830f21bc28e6fae.

--- server/api/tvepisode/index.js
+++ server/api/tvepisode/index.js
@@ -1,17 +1,15 @@
 'use strict';
 
-import {Router} from 'express';
-import * as controller from './tvepisode.controller';
-import * as auth from '../../auth/auth.service';
+var express = require('express');
+var controller = require('./tvepisode.controller');
 
-var router = new Router();
+var router = express.Router();
 
 router.get('/', controller.index);
-router.get('/:id', auth.isAuthenticated(), controller.show);
+router.get('/:id', controller.show);
 router.post('/', controller.create);
-router.delete('/:id', auth.hasRole('admin'), controller.destroy);
-// router.get('/me', auth.isAuthenticated(), controller.me);
-router.put('/:id/seen', auth.isAuthenticated(), controller.update);
-// router.patch('/:id/url', auth.isAuthenticated(), controller.update);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
--- server/api/tvshow/index.js
+++ server/api/tvshow/index.js
@@ -1,18 +1,15 @@
 'use strict';
 
+var express = require('express');
+var controller = require('./tvshow.controller');
 
-
-import {Router} from 'express';
-import * as controller from './tvshow.controller';
-import * as auth from '../../auth/auth.service';
-
-var router = new Router();
+var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', auth.isAuthenticated(), controller.create);
-router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.isAuthenticated(), controller.update);
-router.delete('/:id', auth.hasRole('admin'), controller.destroy);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
